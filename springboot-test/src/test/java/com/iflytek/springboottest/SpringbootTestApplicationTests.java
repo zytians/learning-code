01be1708a2e10636a3e9d27f923cb4233ebfee46
zytian <zytian1869@163.com>
2020-04-08 08:18:17 +0800

    loading file and directory first

---
+++ springboot-test/src/test/java/com/iflytek/springboottest/SpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iflytek.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
